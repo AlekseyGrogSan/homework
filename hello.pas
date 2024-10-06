df5d7a7ed98c37aa22ef8c0361c27c6a0c8e1527
Aleksey <alexkernel05@gamil.com>
2024-10-06 11:21:32 +0300

    helloCommit

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿##
+var lang:= '';
+readln(lang);
+
+if lang = 'Английский' then 
+  print('Hello!')
+else if lang = 'Русский' then 
+  print('Привет!')
+else if lang = 'Испанский' then
+  print('Ola!');
